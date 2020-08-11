0b1f0924dcbb0d6ed29fe8202bc00bfa8c321ea5
Ariel Cacho Mendoza <arielcm@gmail.com>
2020-08-11 01:03:46 -0300

    Commit inicial

---
+++ src/SampleWebApi/HealthChecks/ChangeByMinuteHealthCheck.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SampleWebApi.HealthChecks
+{
+    public class ChangeByMinuteHealthCheck : IHealthCheck
+    {
+        public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+        {
+            int number = DateTime.UtcNow.Minute % 3;
+            switch (number)
+            {
+                case 0:
+                    return await Task.FromResult(HealthCheckResult.Healthy());
+                case 1:
+                    return await Task.FromResult(HealthCheckResult.Degraded());
+                default:
+                    return await Task.FromResult(HealthCheckResult.Unhealthy());
+            }
+        }
+    }
+}
---
+++ src/SampleWebApi/Startup.cs
@@ -0,0 +1,56 @@
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using SampleWebApi.HealthChecks;
+
+namespace SampleWebApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddHealthChecks()
+                .AddCheck("Self", new AlwaysOkHealthCheck())
+                .AddCheck<ChangeByMinuteHealthCheck>("random1", tags: new[] { "random" });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                endpoints.MapHealthChecks("/hc"); // Summary
+
+                // Detail
+                endpoints.MapHealthChecks("/hcd", new HealthCheckOptions
+                {
+                    Predicate = _ => true,
+                    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+                });
+            });
+        }
+    }
+}
